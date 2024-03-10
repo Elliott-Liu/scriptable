0013ba5620afe88dd13118587bc26488c87aef07
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-10 23:45:10 +0000

    Add openInScriptable()

--- scripts/scriptable.lua
+++ scripts/scriptable.lua
@@ -9,9 +9,26 @@ distributablePath = PWD .. "/dist/"
 buildPath = PWD .. "/build/"
 modulesPath = PWD .. "/modules/"
 iCloudPath = HOME .. "/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/"
+devName = " (DEV)"
 
 function init(file)
   print(utilities.getFilePath(iCloudPath .. file))
 end
 
-init("Scriptable World.ts")
+function openInScriptable(filePath)
+  local fileBasename = utilities.getBasename("Days Until.js")
+  local fileName = utilities.replaceFileExtension(fileBasename, "")
+  local uri = utilities.uriEncode(fileName .. devName)
+
+  local openCmd = "open scriptable:///open/" .. uri
+  print("Running command: \"" .. openCmd .. "\".")
+  local openResult = os.execute(openCmd)
+
+  if openResult == true then
+    local runCmd = "open scriptable:///run/" .. uri
+    print("Running command: \"" .. runCmd .. "\".")
+    os.execute(runCmd)
+  end
+end
+
+openInScriptable("Scriptable World.ts")
