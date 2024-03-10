7711c2908405b367ea03390704fa0097d20256d5
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2024-03-10 23:23:58 +0000

    Add getBasename and replaceFileExtension

--- scripts/utilities.lua
+++ scripts/utilities.lua
@@ -16,6 +16,33 @@ function utilities.getFilePath(filePath)
   return filePath:match("(.+)/[^/]*$")
 end
 
+function utilities.getBasename(filePath)
+  local name = string.gsub(filePath, "(.*/)(.*)", "%2")
+  return name
+end
+
+function utilities.replaceFileExtension(filePath, extension)
+  local name = utilities.getBasename(filePath)
+  local basename = string.match(name, "(.+)%..+") or name
+
+  if extension == nil or extension == "" then
+    return filePath:gsub(name .. "$", basename)
+  else
+    if string.sub(extension, 1, 1) ~= "." then
+      extension = "." .. extension
+    end
+    return filePath:gsub(name .. "$", basename .. extension)
+  end
+end
+
+function utilities.uriEncode(string)
+  string = string.gsub(string, "([^%w _%%%-%.~])", function(c)
+    return string.format("%%%02X", string.byte(c))
+  end)
+  string = string.gsub(string, " ", "+")
+  return string
+end
+
 function utilities.logError(message)
   print("Error: " .. message)
 end
